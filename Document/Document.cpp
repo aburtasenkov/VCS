5ae07d4d21563646340f4c5facc746b0a8f05a40
aburtasenkov <59296596+aburtasenkov@users.noreply.github.com>
2024-11-15 15:01:19 +0100

    Document.cpp -- Add possibility of initializing Document obj with filesystem::path obj

--- Document/Document.cpp
+++ Document/Document.cpp
@@ -13,6 +13,13 @@ DocumentClass::Document::Document(std::istream& is)
     }
 }
 
+DocumentClass::Document::Document(const std::filesystem::path& path_to_input_file)
+{
+    std::string input = static_cast<std::string>(path_to_input_file);
+    std::ifstream ifs{input};
+    *this = DocumentClass::Document(ifs);
+}
+
 DocumentClass::Document& DocumentClass::Document::operator=(DocumentClass::Document&& other)
 {
     lines = std::move(other.lines);
