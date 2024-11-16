64622ccd2066f915c69d46a16724807af5adbcdd
aburtasenkov <aburtasenkov@gmail.com>
2024-11-16 12:13:34 +0100

    DocumentComparison.hpp -- Comments for unterstandability

--- DocumentComparison.hpp
+++ DocumentComparison.hpp
@@ -10,19 +10,25 @@ namespace DocumentComparisonClass
     };
 
     bool is_document_changed(DocumentClass::Document& original_state, DocumentClass::Document& changed_state)
+    // return true if a document was changed
+    // return false if the document stayed the same
     {
+        // same amount of lines?
         if (original_state.size() != changed_state.size())
             return true;
 
+        // iterate through each line
         for (int index_line = 0; index_line < original_state.size(); ++index_line)
         {
-            int original_state_words_count = original_state[index_line].get_container().size();
-            int changed_state_words_count = changed_state[index_line].get_container().size();
+            int original_state_line_words_count = original_state[index_line].get_container().size();
+            int changed_state_line_words_count = changed_state[index_line].get_container().size();
 
-            if (original_state_words_count != changed_state_words_count)
+            // same amount of words?
+            if (original_state_line_words_count != changed_state_line_words_count)
                 return true;
 
-            for (int index_word = 0; index_word < original_state_words_count; ++index_word)
+            //  iterate and compare each word together
+            for (int index_word = 0; index_word < original_state_line_words_count; ++index_word)
             {
                 if (original_state[index_line][index_word] != changed_state[index_line][index_word])
                     return true;
