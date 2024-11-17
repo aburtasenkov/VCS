45556bc721a6b6acde407e368e29e1d437555896
aburtasenkov <aburtasenkov@gmail.com>
2024-11-17 23:55:52 +0100

    removed not needed objects

--- DocumentComparison.cpp
+++ DocumentComparison.cpp
@@ -3,9 +3,6 @@
 DocumentComparisonClass::DocumentComparison::DocumentComparison(DocumentClass::Document& original_state, DocumentClass::Document& changed_state)
 // Compares 2 documents for changes on initialization
 {
-    std::vector<std::pair<LineClass::Line, int>> temp_added;
-    std::vector<int> temp_removed;
-
     // iterate through each line
     for (int index_line = 0; index_line < original_state.size(); ++index_line)
     {
@@ -15,24 +12,20 @@ DocumentComparisonClass::DocumentComparison::DocumentComparison(DocumentClass::D
         // Not same amount of words
         if (original_state_line_words_count != changed_state_line_words_count)
         {
-            temp_added.push_back({changed_state[index_line], index_line});
-            temp_removed.push_back(index_line);
+            added_lines.push_back({changed_state[index_line], index_line});
+            removed_lines.push_back(index_line);
             continue;
         }
 
-        std::cout << original_state_line_words_count << "\n";
         //  iterate and compare each word together
         for (int index_word = 0; index_word < original_state_line_words_count; ++index_word)
         {
             if (original_state[index_line][index_word] != changed_state[index_line][index_word])
             {
-                temp_added.push_back({changed_state[index_line], index_line});
-                temp_removed.push_back(index_line);
+                added_lines.push_back({changed_state[index_line], index_line});
+                removed_lines.push_back(index_line);
                 break;  // Go to next line
             }
         }
     }
-    added_lines = temp_added;
-    removed_lines = temp_removed;
-    std::cout << added_lines.size() << "\t" << removed_lines.size();
 }
