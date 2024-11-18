4c671f41e1dc93e692c32cddc775cae2b062d15b
aburtasenkov <aburtasenkov@gmail.com>
2024-11-18 02:04:35 +0100

    DocumentComparison Class is_inserted removed indexing of lines

--- DocumentComparison.cpp
+++ DocumentComparison.cpp
@@ -4,17 +4,15 @@ DocumentComparisonClass::DocumentComparison::DocumentComparison(DocumentClass::D
 // Compares 2 documents for changes on initialization
 {
     // iterate through each line
-    for (int index_line = 0; index_line < original_state.size(); ++index_line)
+
+    for (int index_line = 0; index_line < std::min(original_state.size(), changed_state.size()); ++index_line)
     {
-        if (!(original_state[index_line] == changed_state[index_line]))
-        {
-            added_lines.push_back({changed_state[index_line], index_line});
-            removed_lines.push_back(index_line);
-        }
+        added_lines.push_back({changed_state[index_line], index_line});
+        removed_lines.push_back(index_line);
     }
 }
 
-bool DocumentComparisonClass::is_inserted(DocumentClass::Document::iterator b, DocumentClass::Document::iterator e, LineClass::Line& line, int index)
+bool DocumentComparisonClass::is_inserted(DocumentClass::Document::iterator b, DocumentClass::Document::iterator e, LineClass::Line& line)
 // Possibly wrong code idk didnt check
 {
     while (b != e)
