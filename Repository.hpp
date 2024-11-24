ad9a046bfab5db212cd3afe1d54989c8d315999e
aburtasenkov <aburtasenkov@gmail.com>
2024-11-24 21:16:06 +0100

    Made cache for VCS -- not complete yet, need to make operator>> for Repository

--- Repository.hpp
+++ Repository.hpp
@@ -7,7 +7,8 @@ public:
     Repository(const std::string& repo_name);
 
     void add_commit(const Commit_Namespace::Commit& com);
-    friend std::istream& operator>>(std::istream& is, const Repository& repo);
+    friend std::istream& operator>>(std::istream& is, Repository& repo);
+    friend std::ostream& operator<<(std::ostream& os, const Repository& repo);
 
     const std::string& get_name() { return name; };
     std::vector<Commit_Namespace::Commit>& get_commits() { return commits; }
@@ -25,8 +26,19 @@ void Repository::add_commit(const Commit_Namespace::Commit& com)
     commits.push_back(com);
 }
 
-std::istream& operator>>(std::istream& is, const Repository& repo)
+std::istream& operator>>(std::istream& is, Repository& repo)
 {
     is >> repo.name;
+    // Input already made commits that are saved in cache
     return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const Repository& repo)
+{
+    os << repo.name << "\n";;
+    for (auto& commit : repo.commits)
+    {
+        os << commit << "\n";
+    }
+    return os;
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ const std::filesystem::path CURRENT_PATH{std::filesystem::current_path()};
 
 // filepath in relation to CURRENT_PATH
 const std::filesystem::path VCS_PATH{"VCS/"}; 
-const std::filesystem::path VCS_COMMITED_STATE{"commited_state/"};
+const std::filesystem::path VCS_COMMITED_STATE{"commited state/"};
 const std::filesystem::path VCS_STAGED_STATE{"staged state/"};
 const std::filesystem::path VCS_IGNORE{"ignore"};
 const std::filesystem::path VCS_CACHE{"cache"};
@@ -22,38 +22,38 @@ const int MIN_INPUT_ARGUMENTS = 2;
 const std::string CURRENT_FILENAME = "main.cpp";
 
 void copy_file_to_directory(const std::filesystem::path& source, const std::filesystem::path& copy_directory);
-void create_repo_cache(const std::string& repository_name);
 Repository* read_cache();
 void initialize(const std::string& repository_name);
-void add(Repository* repo, const std::filesystem::path& source_path);
 void commit(Repository* repo, const std::string& commit_message);
 
 void copy_file_to_directory(const std::filesystem::path& source, const std::filesystem::path& copy_directory)
 {
     if (!std::filesystem::exists(source))
-        throw Exception{"Missing File Error: File to stage does not exist in directory", {"main.cpp", 64}};
-    Document_Namespace::Document doc{source};
-    std::ofstream ofs {copy_directory/source};
-    if (!ofs.good()) 
-        throw Exception{"Reading-Error: could not open file to write", {"main.cpp", 67}};
-    ofs << doc;
-}
-
-void create_repo_cache(const std::string& repository_name)
-// initialize cache files for a repository
-{
-    std::ofstream ofs{CURRENT_PATH/VCS_PATH/VCS_CACHE};
-    ofs << repository_name << "\n";
+        throw Exception{"Missing File Error: File to stage does not exist in directory", {"main.cpp", 34}};
+    std::filesystem::copy(source, copy_directory);
 }
 
 Repository* read_cache()
 {
     if (!std::filesystem::exists(CURRENT_PATH/VCS_PATH/VCS_CACHE))
-        throw Exception{"Missing File Error: Repository uninitialized", {"main.cpp", 80}};
+        throw Exception{"Missing File Error: Repository uninitialized", {"main.cpp", 48}};
+
+    std::ifstream ifs{CURRENT_PATH/VCS_PATH/VCS_CACHE};
 
     Repository* repo = new Repository{};
 
+    ifs >> (*repo);
+
+    ifs.close();
+
     return repo;
+}
+
+void save_cache(Repository* repo)
+{
+    std::ofstream ofs{CURRENT_PATH/VCS_PATH/VCS_CACHE};
+    ofs << (*repo);
+    ofs.close();
 }
 
 void initialize(const std::string& repository_name)
@@ -66,21 +66,59 @@ void initialize(const std::string& repository_name)
 
     // cache for repository data
     if (std::filesystem::exists(CURRENT_PATH/VCS_PATH/VCS_CACHE))
-        throw Exception{"Invalid Initialization: Repository already initialized", {"main.cpp", 100}};
-    create_repo_cache(repository_name);
+        throw Exception{"Invalid Initialization: Repository already initialized", {"main.cpp", 71}};
+    Repository repo{repository_name};
+    save_cache(&repo);
 
     std::ofstream ofs {CURRENT_PATH/VCS_PATH/VCS_IGNORE}; // Create VCS ignore file
+    ofs.close();
 }
 
-void add(Repository* repo, const std::filesystem::path& source_path)
+void add(const std::filesystem::path& source_path)
 // Move file to staged state
 {
     copy_file_to_directory(source_path, CURRENT_PATH/VCS_PATH/VCS_STAGED_STATE);
 }
 
+bool already_commited(const std::filesystem::path& filepath)
+// returns true if both there are any files in commited state that have the same filename
+{
+    for (auto& commited_filepath : std::filesystem::directory_iterator{CURRENT_PATH/VCS_PATH/VCS_COMMITED_STATE})
+    {
+        if (commited_filepath.path().filename() == filepath.filename())
+            return true;
+    }
+    return false;
+}
+
 void commit(Repository* repo, const std::string& commit_message)
 {
+    Commit_Namespace::Commit commit{};
+    for (auto& filepath_iterator : std::filesystem::directory_iterator{CURRENT_PATH/VCS_PATH/VCS_STAGED_STATE})
+    {
+        if (!already_commited(filepath_iterator.path()))
+        {
+            copy_file_to_directory(filepath_iterator.path(), CURRENT_PATH/VCS_PATH/VCS_COMMITED_STATE);
+            return;
+        }
 
+        std::filesystem::path commited_filepath{filepath_iterator.path().filename()};
+
+        Commit_Namespace::Filechange fc{
+                                        Document_Namespace::Document{CURRENT_PATH/VCS_PATH/VCS_COMMITED_STATE/commited_filepath},
+                                        Document_Namespace::Document{filepath_iterator.path()}
+                                        };
+
+        if (fc.changes.is_modified())
+        {
+            commit.push_back(fc);
+        }
+    }
+    repo->add_commit(commit);
+
+    if (!std::filesystem::exists(CURRENT_PATH/VCS_PATH/VCS_CACHE))
+        throw Exception{"Missing File Error: Repository uninitialized", {"main.cpp", 126}};
+    save_cache(repo);
 }
 
 int main(int argc, char** argv)
@@ -88,21 +126,23 @@ try
 {    
     // argv should at least contain a string after executable name
     if (argc < MIN_INPUT_ARGUMENTS)
-        throw Exception{"Syntax-Error: Bad Command Line Input", {CURRENT_FILENAME, 15}};
+        throw Exception{"Syntax-Error: Bad Command Line Input", {CURRENT_FILENAME, 126}};
 
     std::string INPUT_CURRENT_COMMAND = argv[INPUT_COMMAND_INDEX];
 
     if (INPUT_CURRENT_COMMAND == INIT)
         initialize(argv[INPUT_COMMAND_INDEX + 1]);
+    else 
+    {    
+        Repository* repo = read_cache();
 
-    Repository* repo = read_cache();
+        if (INPUT_CURRENT_COMMAND == ADD)
+            add(argv[INPUT_COMMAND_INDEX + 1]);
+        if (INPUT_CURRENT_COMMAND == COMMIT)
+            commit(repo, argv[INPUT_COMMAND_INDEX + 1]);
 
-    if (INPUT_CURRENT_COMMAND == ADD)
-        add(repo, argv[INPUT_COMMAND_INDEX + 1]);
-    if (INPUT_CURRENT_COMMAND == COMMIT)
-        commit(repo, argv[INPUT_COMMAND_INDEX + 1]);
-
-    delete repo;
+        delete repo;
+    }
 
     return 0;
 }
@@ -110,4 +150,5 @@ catch (Exception& except)
 {
     std::cerr << except.what() << std::endl
     << "\t" << except.location().first << ":" << except.location().second << std::endl;
+    return 1;
 }
