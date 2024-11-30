8a2fc068ff10ce4b067498042ce3671c2d86326c
aburtasenkov <aburtasenkov@gmail.com>
2024-11-30 15:14:57 +0100

    Added function to get last line of istream

--- iostream_helper_functions.cpp
+++ iostream_helper_functions.cpp
@@ -62,3 +62,8 @@ std::string ignore_char(const std::string& str, char to_be_ignored)
     }
     return char_free;
 }
+
+void get_last_line(std::istream& is, std::string& str)
+{
+    while(std::getline(is, str));
+}
